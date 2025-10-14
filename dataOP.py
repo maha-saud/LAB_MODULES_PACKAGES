0acb8bce669e63bdbabb84c8e797d43cfd714806
Maha Saud <al-maha2244@hotmail.com>
2025-10-14 21:46:43 +0300

    Maha Saud

---
+++ dataOP.py
@@ -0,0 +1,5 @@
+from datetime import date
+
+def print_current_date():
+    current_date = date.today()
+    print(f"Today's date is: {current_date}")
---
+++ main.py
@@ -0,0 +1,3 @@
+import dataOP 
+
+dataOP.print_current_date()
